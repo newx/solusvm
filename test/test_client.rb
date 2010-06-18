37f6ad4cf9f8d8ff5c5e55c8df2036a183c6f15b
Justin Mazzi <hh@mailheist.com>
2010-06-18 15:38:40 -0400

    Adding client test

---
+++ test/test_client.rb
@@ -0,0 +1,35 @@
+require 'helper'
+
+class TestBase < Test::Unit::TestCase
+
+  def setup
+    setup_solusvm
+    FakeWeb.allow_net_connect = false
+    FakeWeb.clean_registry
+    @client = Solusvm::Client.new
+  end
+
+  def test_create
+    options = {:username => 'vps123', :password=> '123456', :email=> 'email@address.com', :firstname => 'phill', :lastname => 'smith'}
+    FakeWeb.register_uri(:get, "#{base_uri}&action=client-create&#{options.to_query}", :body => load_response('client_create_success'))
+    assert @client.create(options)
+
+    params = @client.returned_parameters
+    assert_equal options[:username], params['username']
+    assert_equal options[:firstname], params['firstname']
+    assert_equal options[:lastname], params['lastname']
+    assert_equal options[:password], params['password']
+    assert_equal options[:email], params['email']
+    assert_equal 'Successfully added client', params['statusmsg']
+    assert_equal 'success', params['status']
+  end
+
+  def test_create_fail
+    FakeWeb.register_uri(:get, "#{base_uri}&action=client-create", :body => load_response('client_create_error'))
+    begin
+      @client.create
+    rescue Solusvm::SolusvmError => e
+      assert_equal 'Empty username field', e.message
+    end
+  end
+end
