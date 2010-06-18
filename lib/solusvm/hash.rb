ff7eed7dcec4fdb86aa87b87a76644e6bb118e10
Justin Mazzi <hh@mailheist.com>
2010-06-18 11:05:04 -0400

    Avoid overriding Hash methods when it's not necessary

--- lib/solusvm/hash.rb
+++ lib/solusvm/hash.rb
@@ -1,17 +1,23 @@
 class Hash
-  def to_query
-    elements = []
-    self.each do |key, value|
-      elements << "#{CGI::escape(key.to_s)}=#{CGI::escape(value.to_s)}"
+  unless method_defined?(:to_query)
+    def to_query
+      elements = []
+      self.each do |key, value|
+        elements << "#{CGI::escape(key.to_s)}=#{CGI::escape(value.to_s)}"
+      end
+      elements.join('&')
     end
-    elements.join('&')
   end
 
-  def reverse_merge(other_hash)
-    other_hash.merge(self)
+  unless method_defined?(:reverse_merge)
+    def reverse_merge(other_hash)
+      other_hash.merge(self)
+    end
   end
 
-  def reverse_merge!(other_hash)
-    replace(reverse_merge(other_hash))
+  unless method_defined?(:reverse_merge!)
+    def reverse_merge!(other_hash)
+      replace(reverse_merge(other_hash))
+    end
   end
 end
