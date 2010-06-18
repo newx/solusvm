aaa28fb11dec39c80f7dd8863334c2528079e70f
Justin Mazzi <hh@mailheist.com>
2010-06-18 09:31:57 -0400

    Adding validate_server_type Add requires for new classes

--- lib/solusvm.rb
+++ lib/solusvm.rb
@@ -4,6 +4,8 @@ require 'rubygems'
 require 'xmlsimple'
 
 module Solusvm
+  VALID_SERVER_TYPES = ['openvz', 'xen', 'xen hvm']
+
   def self.config(api_id, api_key, options={})
     @api_id       = api_id
     @api_key      = api_key
@@ -14,13 +16,22 @@ module Solusvm
   def self.api_endpoint
     @api_endpoint
   end
-  
+
   def self.api_endpoint=(value)
     @api_endpoint = value
   end
+
+  def self.validate_server_type!(type)
+    unless VALID_SERVER_TYPES.include?(type)
+      raise SolusvmError, "Invalid Virtual Server type: #{type}"
+    end
+  end
+
 end
 
 directory = File.expand_path(File.dirname(__FILE__))
+require File.join(directory, 'solusvm', 'exceptions')
 require File.join(directory, 'solusvm', 'hash')
 require File.join(directory, 'solusvm', 'base')
+require File.join(directory, 'solusvm', 'client')
 require File.join(directory, 'solusvm', 'server')
