869d6765a75645b34aafb2e6c0776d15d69fc1ac
Justin Mazzi <hh@mailheist.com>
2010-06-18 10:51:18 -0400

    Change some options around for create Change vserver to vserverid Fix typo

--- lib/solusvm/server.rb
+++ lib/solusvm/server.rb
@@ -1,41 +1,41 @@
 module Solusvm
   class Server < Base
-    def create(hostname, password, client, options = {})
+    def create(hostname, password, options = {})
       options.reverse_merge!(
         :type         => 'xen',
-        :username     => client,
+        :username     => nil,
         :ips          => 1,
         :node         => nil,
         :plan         => nil,
         :template     => nil,
-        :rootpassword => password,
+        :password     => password,
         :hostname     => hostname
       ).merge!(:action => 'vserver-create')
       perform_request(options)
     end
 
     def boot(vid)
-      perform_request(:action => 'vserver-boot', :vserver => vid)
+      perform_request(:action => 'vserver-boot', :vserverid => vid)
     end
 
     def reboot(vid)
-      perform_request(:action => 'vserver-reboot', :vserver => vid)
+      perform_request(:action => 'vserver-reboot', :vserverid => vid)
     end
 
     def suspend(vid)
-      perform_request(:action => 'vserver-suspend', :vserver => vid)
+      perform_request(:action => 'vserver-suspend', :vserverid => vid)
     end
 
     def resume(vid)
-      perform_request(:action => 'vserver-unsuspend', :vserver => vid)
+      perform_request(:action => 'vserver-unsuspend', :vserverid => vid)
     end
 
     def shutdown(vid)
-      perform_request(:action => 'vserver-shutdown', :vserver => vid)
+      perform_request(:action => 'vserver-shutdown', :vserverid => vid)
     end
 
     def terminate(vid, deleteclient = false)
-      perform_request(:action => 'vserver-shutdown', :vserver => vid, :deleteclient => deleteclient)
+      perform_request(:action => 'vserver-terminate', :vserverid => vid, :deleteclient => deleteclient)
     end
 
     def exists?(vid)
@@ -49,11 +49,11 @@ module Solusvm
     end
 
     def add_ip(vid)
-      perform_request(:action => 'vserver-addip', :vserver => vid)
+      perform_request(:action => 'vserver-addip', :vserverid => vid)
     end
 
     def change_plan(vid, plan)
-      perform_request(:action => 'vserver-change', :vserver => vid, :plan => plan)
+      perform_request(:action => 'vserver-change', :vserverid => vid, :plan => plan)
     end
   end
 end
