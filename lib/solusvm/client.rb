fdb7115cd9c10bc13819b8bf2bb28a30ade3ec3c
Justin Mazzi <hh@mailheist.com>
2010-06-18 09:33:18 -0400

    Add Client

---
+++ lib/solusvm/client.rb
@@ -0,0 +1,13 @@
+module Solusvm
+  class Client < Base
+    # username   [username]
+    # password   [password]
+    # email      [email address]
+    # firstname  [first name]
+    # lastname   [last name]
+    def create(options)
+      options.merge!(:action => 'client-create')
+      perform_request(options)
+    end
+  end
+end
