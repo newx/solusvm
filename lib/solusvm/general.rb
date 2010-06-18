b0f896d1047275427e71f4033a5bb1da37e77a26
Justin Mazzi <hh@mailheist.com>
2010-06-18 09:34:39 -0400

    Add General. This methods have no real home ATM

---
+++ lib/solusvm/general.rb
@@ -0,0 +1,22 @@
+module Solusvm
+  class General < Base
+    def nodes(type)
+      type.strip!
+      validate_server_type!(type)
+      perform_request(:action => 'listnodes', :type => type)
+      returned_paramaters['nodes']
+    end
+
+    def templates(type)
+      validate_server_type!(type)
+      perform_request(:action => 'listtemplates', :type => type)
+      returned_paramaters['templates']
+    end
+
+    def node_statistics(nodeid)
+      perform_request(:action => 'node-statistics', :nodeid => nodeid)
+      returned_paramaters
+    end
+
+  end
+end
