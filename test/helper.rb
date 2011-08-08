b4034f49e45754e09318ac9c961c4d0e79218429
Joshua Priddle <jpriddle@nevercraft.net>
2011-08-08 14:11:32 -0400

    No need to require RubyGems or tweak load path

--- test/helper.rb
+++ test/helper.rb
@@ -1,8 +1,5 @@
-require 'rubygems'
 require 'test/unit'
 
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 # Use TURN if available
 begin
   require 'turn'
