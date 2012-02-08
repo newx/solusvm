b9d19aa74e39589a5d7b99a9f10307a88556005d
Fabio Kreusch <fabiokr@gmail.com>
2012-02-08 17:49:19 -0200

    Added reseller-cli

--- lib/solusvm/cli.rb
+++ lib/solusvm/cli.rb
@@ -3,12 +3,14 @@ require 'solusvm/cli/base_cli'
 require 'solusvm/cli/server_cli'
 require 'solusvm/cli/node_cli'
 require 'solusvm/cli/general_cli'
+require 'solusvm/cli/reseller_cli'
 
 module Solusvm
   class Cli < Thor
     register(ServerCli,  'server', 'server <command>', 'Server commands')
     register(NodeCli,    'node', 'node <command>', 'Node commands')
     register(GeneralCli, 'general', 'general <command>', 'General commands')
+    register(ResellerCli, 'reseller', 'reseller <command>', 'Reseller commands')
 
     desc "version", "Outputs the current program version"
     def version
---
+++ test/cli/test_reseller_cli.rb
@@ -0,0 +1,139 @@
+require 'helper'
+require 'solusvm/cli'
+
+class TestResellerCli < Test::Unit::TestCase
+
+  def setup
+    # Prevents mocha from stubbing non existent methods so that we now if the CLI is failing because
+    # something was moved around.
+    Mocha::Configuration.prevent(:stubbing_non_existent_method)
+  end
+
+  def test_should_delegate_reseller_create_to_reseller
+    Solusvm.expects(:config).with("thelogin", "thekey", { :url => "theurl" })
+    Solusvm::Reseller.stubs(:new => mock{ expects(:create).with() do |options|
+      expected = {
+        :username => "theusername",
+        :password => "thepassword",
+        :email => "theemail",
+        :firstname => "thefirstname",
+        :lastname => "thelastname",
+        :company => "thecompany",
+        :usernameprefix => "theusernameprefix",
+        :maxvps => "themaxvps",
+        :maxusers => "themaxusers",
+        :maxmem => "themaxmem",
+        :maxburst => "themaxburst",
+        :maxdisk => "themaxdisk",
+        :maxbw => "themaxbw",
+        :maxipv4 => "themaxipv4",
+        :maxipv6 => "themaxipv6",
+        :nodegroups => "thenodegroups",
+        :mediagroups => "themediagroups",
+        :openvz => "theopenvz",
+        :xenpv => "thexenpv",
+        :xenhvm => "thexenhvm",
+        :kvm => "thekvm"
+      }
+
+      expected.all? { |k,v| options[k] == v }
+
+      end.returns("theresult") 
+    })
+
+    $stdout.expects(:puts).with("theresult")
+    Solusvm::Cli.start(cli_expand_base_arguments([
+      "reseller", "create", 
+      "--username", "theusername",
+      "--password", "thepassword",
+      "--email", "theemail",
+      "--firstname", "thefirstname",
+      "--lastname", "thelastname",
+      "--company", "thecompany",
+      "--usernameprefix", "theusernameprefix",
+      "--maxvps", "themaxvps",
+      "--maxusers", "themaxusers",
+      "--maxmem", "themaxmem",
+      "--maxburst", "themaxburst",
+      "--maxdisk", "themaxdisk",
+      "--maxbw", "themaxbw",
+      "--maxipv4", "themaxipv4",
+      "--maxipv6", "themaxipv6",
+      "--nodegroups", "thenodegroups",
+      "--mediagroups", "themediagroups",
+      "--openvz", "theopenvz",
+      "--xenpv", "thexenpv",
+      "--xenhvm", "thexenhvm",
+      "--kvm", "thekvm"
+    ]))
+  end
+
+  def test_should_delegate_reseller_change_resources_to_reseller
+    Solusvm.expects(:config).with("thelogin", "thekey", { :url => "theurl" })
+    Solusvm::Reseller.stubs(:new => mock{ expects(:change_resources).with() do |options|
+      expected = {
+        :maxvps => "themaxvps",
+        :maxusers => "themaxusers",
+        :maxmem => "themaxmem",
+        :maxburst => "themaxburst",
+        :maxdisk => "themaxdisk",
+        :maxbw => "themaxbw",
+        :maxipv4 => "themaxipv4",
+        :maxipv6 => "themaxipv6",
+        :nodegroups => "thenodegroups",
+        :mediagroups => "themediagroups",
+        :openvz => "theopenvz",
+        :xenpv => "thexenpv",
+        :xenhvm => "thexenhvm",
+        :kvm => "thekvm"
+      }
+
+      expected.all? { |k,v| options[k] == v }
+
+      end.returns("theresult") 
+    })
+
+    $stdout.expects(:puts).with("theresult")
+    Solusvm::Cli.start(cli_expand_base_arguments([
+      "reseller", "change-resources", 
+      "--maxvps", "themaxvps",
+      "--maxusers", "themaxusers",
+      "--maxmem", "themaxmem",
+      "--maxburst", "themaxburst",
+      "--maxdisk", "themaxdisk",
+      "--maxbw", "themaxbw",
+      "--maxipv4", "themaxipv4",
+      "--maxipv6", "themaxipv6",
+      "--nodegroups", "thenodegroups",
+      "--mediagroups", "themediagroups",
+      "--openvz", "theopenvz",
+      "--xenpv", "thexenpv",
+      "--xenhvm", "thexenhvm",
+      "--kvm", "thekvm"
+    ]))
+  end
+
+  def test_should_delegate_reseller_info_to_reseller
+    Solusvm.expects(:config).with("thelogin", "thekey", { :url => "theurl" })
+    Solusvm::Reseller.stubs(:new => mock{ expects(:info).with("theusername").returns("theresult") })
+
+    $stdout.expects(:puts).with("theresult")
+    Solusvm::Cli.start(cli_expand_base_arguments(["reseller", "info", "theusername"]))
+  end
+
+  def test_should_delegate_reseller_delete_to_reseller
+    Solusvm.expects(:config).with("thelogin", "thekey", { :url => "theurl" })
+    Solusvm::Reseller.stubs(:new => mock{ expects(:delete).with("theusername").returns("theresult") })
+
+    $stdout.expects(:puts).with("theresult")
+    Solusvm::Cli.start(cli_expand_base_arguments(["reseller", "delete", "theusername"]))
+  end
+
+  def test_should_delegate_reseller_list_to_reseller
+    Solusvm.expects(:config).with("thelogin", "thekey", { :url => "theurl" })
+    Solusvm::Reseller.stubs(:new => mock{ expects(:list).returns("theresult") })
+
+    $stdout.expects(:puts).with("theresult")
+    Solusvm::Cli.start(cli_expand_base_arguments(["reseller", "list"]))
+  end  
+end
