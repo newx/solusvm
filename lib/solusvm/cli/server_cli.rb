7134c86c4bd13487b113e73088c3e924c1aa4722
Fabio Kreusch <fabiokr@gmail.com>
2012-02-08 16:51:45 -0200

    Added missing server clis

--- lib/solusvm/cli/server_cli.rb
+++ lib/solusvm/cli/server_cli.rb
@@ -6,9 +6,59 @@ module Solusvm
       say server.status(vserverid)
     end
 
-    desc "changeplan VSERVERID NEWPLAN", "Changes the plan of a server"
-    def changeplan(vserverid, newplan)
+    desc "info VSERVERID", "Retrieves information from a server"
+    def info(vserverid)
+      say server.info(vserverid)
+    end
+
+    desc "vnc VSERVERID", "Retrieves vnc information from a server"
+    def vnc(vserverid)
+      say server.vnc(vserverid)
+    end
+
+    desc "console VSERVERID", "Retrieves console information from a server"
+    def console(vserverid)
+      say server.console(vserverid)
+    end
+
+    desc "info-all VSERVERID", "Retrieves all availavle information from a server"
+    def info_all(vserverid)
+      say server.info_all(vserverid)
+    end
+
+    desc "change-plan VSERVERID NEWPLAN", "Changes the plan of a server"
+    def change_plan(vserverid, newplan)
       say server.change_plan(vserverid, newplan)
+    end
+
+    desc "change-owner VSERVERID CLIENTID", "Changes the owner of a server"
+    def change_owner(vserverid, clientid)
+      say server.change_owner(vserverid, clientid)
+    end
+
+    desc "change-consolepass VSERVERID NEWPASSWORD", "Changes the console password of a server"
+    def change_consolepass(vserverid, newpassword)
+      say server.change_consolepass(vserverid, newpassword)
+    end
+
+    desc "change-vncpass VSERVERID NEWPASSWORD", "Changes the vnc password of a server"
+    def change_vncpass(vserverid, newpassword)
+      say server.change_vncpass(vserverid, newpassword)
+    end
+
+    desc "change-rootpass VSERVERID NEWPASSWORD", "Changes the root password of a server"
+    def change_rootpass(vserverid, newpassword)
+      say server.change_rootpassword(vserverid, newpassword)
+    end
+
+    desc "change-bootorder VSERVERID BOOTORDER", "Changes the boot order of a server [cd(Hard Disk CDROM)|dc(CDROM Hard Disk)|c(Hard Disk)|d(CDROM)]"
+    def change_bootorder(vserverid, newbootorder)
+      say server.change_bootorder(vserverid, newbootorder)
+    end
+
+    desc "change-hostname VSERVERID HOSTNAME", "Changes the hostname of a server"
+    def change_hostname(vserverid, newhostname)
+      say server.change_hostname(vserverid, newhostname)
     end
 
     desc "addip VSERVERID", "Adds an ip to the server"
@@ -57,6 +107,31 @@ module Solusvm
       say server.rebuild(vserverid, {:template => options[:template]})
     end
 
+    desc "tun-switcher VSERVERID SWITCH(on|off)", "Enable/Disable TUN/TAP"
+    def tun_switcher(vserverid, switch)
+      say switch(vserverid, switch, :tun_enable, :tun_disable)
+    end
+
+    desc "network-switcher VSERVERID SWITCH(on|off)", "Enable/Disable Network mode"
+    def network_switcher(vserverid, switch)
+      say switch(vserverid, switch, :network_enable, :network_disable)
+    end
+
+    desc "pae-switcher VSERVERID SWITCH(on|off)", "Enable/Disable PAE"
+    def pae_switcher(vserverid, switch)
+      say switch(vserverid, switch, :pae_enable, :pae_disable)
+    end
+
+    desc "mountiso VSERVERID ISO", "Mounts an iso"
+    def mountiso(vserverid, iso)
+      say server.mountiso(vserverid, iso)
+    end
+
+    desc "unmountiso VSERVERID", "Unmounts an iso"
+    def unmountiso(vserverid)
+      say server.unmountiso(vserverid)
+    end
+
     desc "create HOSTNAME PASSWORD", "Creates a new server"
     method_option :plan, :type => :string, :desc => "Plan to use",  :aliases => ["-p", "--plan"]
     method_option :ips,  :type => :string, :desc => "Number of ips to add to the vps",  :aliases => ["-i", "--ips"]
@@ -65,7 +140,7 @@ module Solusvm
     method_option :template, :type => :string, :desc => "VPS template to boot from",  :aliases => ["-t", "--template"]
     method_option :node, :type => :string, :desc => "Node to provision on",  :aliases => ["-n", "--node"]
     def create(hostname, password)
-      server.create(hostname, password, {
+      say server.create(hostname, password, {
         :plan => options[:plan], :ips => options[:ips], :type => options[:kind], 
         :username => options[:username], :template => options[:template], :node => options[:node]
       })
@@ -77,6 +152,16 @@ module Solusvm
       @server ||= begin
         configure
         Solusvm::Server.new
+      end
+    end
+
+    def switch(vserverid, switch_value, on_method, off_method)
+      if switch_value == "on"
+        server.send(on_method, vserverid)
+      elsif switch_value == "off"
+        server.send(off_method, vserverid)
+      else
+        "Invalid switch value. Valid values are on|off"
       end
     end
   end
--- test/cli/test_server_cli.rb
+++ test/cli/test_server_cli.rb
@@ -17,12 +17,60 @@ class TestServerCli < Test::Unit::TestCase
     Solusvm::Cli.start(cli_expand_base_arguments(["server", "status", "thevserverid"]))
   end
 
-  def test_should_delegate_server_changeplan_to_server
+  def test_should_delegate_server_change_plan_to_server
     Solusvm.expects(:config).with("thelogin", "thekey", { :url => "theurl" })
     Solusvm::Server.stubs(:new => mock{ expects(:change_plan).with("thevserverid", "thenewplan").returns("theresult") })
 
     $stdout.expects(:puts).with("theresult")
-    Solusvm::Cli.start(cli_expand_base_arguments(["server", "changeplan", "thevserverid", "thenewplan"]))
+    Solusvm::Cli.start(cli_expand_base_arguments(["server", "change-plan", "thevserverid", "thenewplan"]))
+  end
+
+  def test_should_delegate_server_change_owner_to_server
+    Solusvm.expects(:config).with("thelogin", "thekey", { :url => "theurl" })
+    Solusvm::Server.stubs(:new => mock{ expects(:change_owner).with("thevserverid", "thenewowner").returns("theresult") })
+
+    $stdout.expects(:puts).with("theresult")
+    Solusvm::Cli.start(cli_expand_base_arguments(["server", "change-owner", "thevserverid", "thenewowner"]))
+  end
+
+  def test_should_delegate_server_change_consolepass_to_server
+    Solusvm.expects(:config).with("thelogin", "thekey", { :url => "theurl" })
+    Solusvm::Server.stubs(:new => mock{ expects(:change_consolepass).with("thevserverid", "thenewpass").returns("theresult") })
+
+    $stdout.expects(:puts).with("theresult")
+    Solusvm::Cli.start(cli_expand_base_arguments(["server", "change-consolepass", "thevserverid", "thenewpass"]))
+  end
+
+  def test_should_delegate_server_change_vncpass_to_server
+    Solusvm.expects(:config).with("thelogin", "thekey", { :url => "theurl" })
+    Solusvm::Server.stubs(:new => mock{ expects(:change_vncpass).with("thevserverid", "thenewpass").returns("theresult") })
+
+    $stdout.expects(:puts).with("theresult")
+    Solusvm::Cli.start(cli_expand_base_arguments(["server", "change-vncpass", "thevserverid", "thenewpass"]))
+  end
+
+  def test_should_delegate_server_change_rootpass_to_server
+    Solusvm.expects(:config).with("thelogin", "thekey", { :url => "theurl" })
+    Solusvm::Server.stubs(:new => mock{ expects(:change_rootpassword).with("thevserverid", "thenewpass").returns("theresult") })
+
+    $stdout.expects(:puts).with("theresult")
+    Solusvm::Cli.start(cli_expand_base_arguments(["server", "change-rootpass", "thevserverid", "thenewpass"]))
+  end
+
+  def test_should_delegate_server_change_bootorder_to_server
+    Solusvm.expects(:config).with("thelogin", "thekey", { :url => "theurl" })
+    Solusvm::Server.stubs(:new => mock{ expects(:change_bootorder).with("thevserverid", "theneworder").returns("theresult") })
+
+    $stdout.expects(:puts).with("theresult")
+    Solusvm::Cli.start(cli_expand_base_arguments(["server", "change-bootorder", "thevserverid", "theneworder"]))
+  end
+
+  def test_should_delegate_server_change_hostname_to_server
+    Solusvm.expects(:config).with("thelogin", "thekey", { :url => "theurl" })
+    Solusvm::Server.stubs(:new => mock{ expects(:change_hostname).with("thevserverid", "thenewhostname").returns("theresult") })
+
+    $stdout.expects(:puts).with("theresult")
+    Solusvm::Cli.start(cli_expand_base_arguments(["server", "change-hostname", "thevserverid", "thenewhostname"]))
   end
 
   def test_should_delegate_server_add_ip_to_server
@@ -96,6 +144,102 @@ class TestServerCli < Test::Unit::TestCase
 
     $stdout.expects(:puts).with("theresult")
     Solusvm::Cli.start(cli_expand_base_arguments(["server", "rebuild", "thevserverid", "--template", "thetemplate"]))
+  end
+
+  def test_should_delegate_server_tun_switcher_on_to_server
+    Solusvm.expects(:config).with("thelogin", "thekey", { :url => "theurl" })
+    Solusvm::Server.stubs(:new => mock{ expects(:tun_enable).with("thevserverid").returns("theresult") })
+
+    $stdout.expects(:puts).with("theresult")
+    Solusvm::Cli.start(cli_expand_base_arguments(["server", "tun-switcher", "thevserverid", "on"]))
+  end
+
+  def test_should_delegate_server_tun_switcher_off_to_server
+    Solusvm.expects(:config).with("thelogin", "thekey", { :url => "theurl" })
+    Solusvm::Server.stubs(:new => mock{ expects(:tun_disable).with("thevserverid").returns("theresult") })
+
+    $stdout.expects(:puts).with("theresult")
+    Solusvm::Cli.start(cli_expand_base_arguments(["server", "tun-switcher", "thevserverid", "off"]))
+  end
+
+  def test_should_delegate_server_network_switcher_on_to_server
+    Solusvm.expects(:config).with("thelogin", "thekey", { :url => "theurl" })
+    Solusvm::Server.stubs(:new => mock{ expects(:network_enable).with("thevserverid").returns("theresult") })
+
+    $stdout.expects(:puts).with("theresult")
+    Solusvm::Cli.start(cli_expand_base_arguments(["server", "network-switcher", "thevserverid", "on"]))
+  end
+
+  def test_should_delegate_server_network_switcher_off_to_server
+    Solusvm.expects(:config).with("thelogin", "thekey", { :url => "theurl" })
+    Solusvm::Server.stubs(:new => mock{ expects(:network_disable).with("thevserverid").returns("theresult") })
+
+    $stdout.expects(:puts).with("theresult")
+    Solusvm::Cli.start(cli_expand_base_arguments(["server", "network-switcher", "thevserverid", "off"]))
+  end
+
+  def test_should_delegate_server_pae_switcher_on_to_server
+    Solusvm.expects(:config).with("thelogin", "thekey", { :url => "theurl" })
+    Solusvm::Server.stubs(:new => mock{ expects(:pae_enable).with("thevserverid").returns("theresult") })
+
+    $stdout.expects(:puts).with("theresult")
+    Solusvm::Cli.start(cli_expand_base_arguments(["server", "pae-switcher", "thevserverid", "on"]))
+  end
+
+  def test_should_delegate_server_pae_switcher_off_to_server
+    Solusvm.expects(:config).with("thelogin", "thekey", { :url => "theurl" })
+    Solusvm::Server.stubs(:new => mock{ expects(:pae_disable).with("thevserverid").returns("theresult") })
+
+    $stdout.expects(:puts).with("theresult")
+    Solusvm::Cli.start(cli_expand_base_arguments(["server", "pae-switcher", "thevserverid", "off"]))
+  end
+
+  def test_should_delegate_server_info_to_server
+    Solusvm.expects(:config).with("thelogin", "thekey", { :url => "theurl" })
+    Solusvm::Server.stubs(:new => mock{ expects(:info).with("thevserverid").returns("theresult") })
+
+    $stdout.expects(:puts).with("theresult")
+    Solusvm::Cli.start(cli_expand_base_arguments(["server", "info", "thevserverid"]))
+  end
+
+  def test_should_delegate_server_vnc_to_server
+    Solusvm.expects(:config).with("thelogin", "thekey", { :url => "theurl" })
+    Solusvm::Server.stubs(:new => mock{ expects(:vnc).with("thevserverid").returns("theresult") })
+
+    $stdout.expects(:puts).with("theresult")
+    Solusvm::Cli.start(cli_expand_base_arguments(["server", "vnc", "thevserverid"]))
+  end
+
+  def test_should_delegate_server_console_to_server
+    Solusvm.expects(:config).with("thelogin", "thekey", { :url => "theurl" })
+    Solusvm::Server.stubs(:new => mock{ expects(:console).with("thevserverid").returns("theresult") })
+
+    $stdout.expects(:puts).with("theresult")
+    Solusvm::Cli.start(cli_expand_base_arguments(["server", "console", "thevserverid"]))
+  end
+
+  def test_should_delegate_server_info_all_to_server
+    Solusvm.expects(:config).with("thelogin", "thekey", { :url => "theurl" })
+    Solusvm::Server.stubs(:new => mock{ expects(:info_all).with("thevserverid").returns("theresult") })
+
+    $stdout.expects(:puts).with("theresult")
+    Solusvm::Cli.start(cli_expand_base_arguments(["server", "info-all", "thevserverid"]))
+  end
+
+  def test_should_delegate_server_mountiso_to_server
+    Solusvm.expects(:config).with("thelogin", "thekey", { :url => "theurl" })
+    Solusvm::Server.stubs(:new => mock{ expects(:mountiso).with("thevserverid", "theiso").returns("theresult") })
+
+    $stdout.expects(:puts).with("theresult")
+    Solusvm::Cli.start(cli_expand_base_arguments(["server", "mountiso", "thevserverid", "theiso"]))
+  end
+
+  def test_should_delegate_server_unmountiso_to_server
+    Solusvm.expects(:config).with("thelogin", "thekey", { :url => "theurl" })
+    Solusvm::Server.stubs(:new => mock{ expects(:unmountiso).with("thevserverid").returns("theresult") })
+
+    $stdout.expects(:puts).with("theresult")
+    Solusvm::Cli.start(cli_expand_base_arguments(["server", "unmountiso", "thevserverid"]))
   end
 
   def test_should_delegate_server_create_to_server
