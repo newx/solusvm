8a44b843420a9eca4a4f8d64de43a91ef5bf479e
tfwright <tfwright@gmail.com>
2012-03-14 10:42:21 -0500

    Refactored/converted node tests to use VCR

--- test/test_node.rb
+++ test/test_node.rb
@@ -2,35 +2,34 @@ require 'helper'
 
 class TestNode < Test::Unit::TestCase
   def setup
-    FakeWeb.allow_net_connect = false
-    FakeWeb.clean_registry
     setup_solusvm
     @nodes = Solusvm::Node.new
   end
 
   def test_list
-    FakeWeb.register_uri(:get, "#{base_uri}&action=listnodes&type=xen", :body => load_response('nodes_list_success'))
-    assert_equal %w(node1 node2 node3 node4), @nodes.list('xen')
+    VCR.use_cassette "node/list" do
+      assert_equal %w(node1 node2 node3 node4), @nodes.list('xen')
+    end
   end
 
   def test_list_empty
-    FakeWeb.register_uri(:get, "#{base_uri}&action=listnodes&type=xen", :body => load_response('error'))
-    assert !@nodes.list('xen')
+    VCR.use_cassette "node/list" do    
+      assert !@nodes.list('openvz')
+    end
   end
 
   def test_nodes_with_invalid_type
-    FakeWeb.register_uri(:get, "#{base_uri}&action=listnodes&type=whatever", :body => load_response('error'))
-    begin
+    assert_raise Solusvm::SolusvmError do
       @nodes.list('whatever')
-      flunk "Shouldn't get here"
-    rescue Solusvm::SolusvmError => e
-      assert e.message.match /Invalid Virtual Server type/
     end
   end
 
   def test_statistics
-    FakeWeb.register_uri(:get, "#{base_uri}&action=node-statistics&nodeid=1", :body => load_response('node_statistics_success'))
-    node_statistics = @nodes.statistics(1)
+    VCR.use_cassette "node/statistics" do
+      @nodes.statistics(1)
+    end
+    
+    node_statistics = @nodes.returned_parameters
     
     assert_equal '1000', node_statistics['freedisk']
     assert_equal '22', node_statistics['sshport']
@@ -48,37 +47,39 @@ class TestNode < Test::Unit::TestCase
   end
 
   def test_list_all_ips_available
-    FakeWeb.register_uri(:get, "#{base_uri}&action=node-iplist&nodeid=1", :body => load_response('node_list_all_ips_available'))
-    assert_equal %w(123.123.123.123 124.124.124.124 125.125.125.125).sort, @nodes.available_ips(1).sort 
+    Solusvm.config("api_id1", api_login[:key], :url => 'http://www.example.com/api')
+    VCR.use_cassette "node/available_ips" do
+      assert_equal %w(123.123.123.123 124.124.124.124 125.125.125.125).sort, @nodes.available_ips(1).sort 
+    end
   end
 
   def test_list_all_ips_not_available
-    FakeWeb.register_uri(:get, "#{base_uri}&action=node-iplist&nodeid=1", :body => load_response('node_list_all_ips_not_available'))
-    assert @nodes.available_ips(1).empty?
+    Solusvm.config("api_id2", api_login[:key], :url => 'http://www.example.com/api')
+    VCR.use_cassette "node/available_ips" do
+      assert @nodes.available_ips(1).empty?
+    end
   end
 
   def test_ids
-    FakeWeb.register_uri(:get, "#{base_uri}&action=node-idlist&type=xen", :body => load_response('nodes_ids_success'))
-    assert_equal %w(nodeid1 nodeid2 nodeid3 nodeid4), @nodes.ids('xen')
+    VCR.use_cassette "node/ids" do
+      assert_equal %w(nodeid1 nodeid2 nodeid3 nodeid4), @nodes.ids('xen')
+    end
   end
 
   def test_nodes_ids_error
-    FakeWeb.register_uri(:get, "#{base_uri}&action=node-idlist&type=whatever", :body => load_response('error'))
-    begin
+    assert_raise Solusvm::SolusvmError do
       @nodes.ids('whatever')
-      flunk "Shouldn't get here"
-    rescue Solusvm::SolusvmError => e
-      assert e.message.match /Invalid Virtual Server type/
     end
   end
 
   def test_virtualservers
-    FakeWeb.register_uri(:get, "#{base_uri}&action=node-virtualservers&nodeid=1", :body => load_response('node_virtualservers_success'))
-
-    servers = @nodes.virtualservers(1)
-    assert_equal 1, servers.size
-
-    server = servers.first
+    Solusvm.config("api_id1", api_login[:key], :url => 'http://www.example.com/api')
+    VCR.use_cassette "node/virtualservers" do
+      @nodes.virtualservers(1)
+    end
+    
+    server = @nodes.returned_parameters["virtualservers"]["virtualserver"].first
+    
     assert_equal "theid", server["vserverid"]
     assert_equal "thexid", server["ctid-xid"]
     assert_equal "theclientid", server["clientid"]
@@ -93,18 +94,25 @@ class TestNode < Test::Unit::TestCase
   end
 
   def test_virtualservers_empty
-    FakeWeb.register_uri(:get, "#{base_uri}&action=node-virtualservers&nodeid=1", :body => load_response('node_virtualservers_success_empty'))
-    assert @nodes.virtualservers(1).empty?
+    Solusvm.config("api_id2", api_login[:key], :url => 'http://www.example.com/api')
+    VCR.use_cassette "node/virtualservers" do
+      assert @nodes.virtualservers(1).empty?
+    end
   end
 
   def test_virtualservers_fail
-    FakeWeb.register_uri(:get, "#{base_uri}&action=node-virtualservers&nodeid=1", :body => load_response('error'))
-    assert_nil @nodes.virtualservers(1)
+    Solusvm.config("api_id3", api_login[:key], :url => 'http://www.example.com/api')
+    VCR.use_cassette "node/virtualservers" do
+      assert_nil @nodes.virtualservers(1)
+    end
   end
 
   def test_xenresources
-    FakeWeb.register_uri(:get, "#{base_uri}&action=node-xenresources&nodeid=1", :body => load_response('node_xenresources_success'))
-    node_resources = @nodes.xenresources(1)
+    VCR.use_cassette "node/xenresources" do
+      @nodes.xenresources(1)
+    end
+    
+    node_resources = @nodes.returned_parameters
     
     assert_equal 'thefreememory', node_resources['freememory']
     assert_equal 'thefreehdd', node_resources['freehdd']
